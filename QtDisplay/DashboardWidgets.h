d686fefda330c94439b13df771271e18fef6b14e
matteusch <matteuschger@gmail.com>
2026-04-25 19:38:10 +0200

    Deleted test files, basic phone screen implementation

--- QtDisplay/DashboardWidgets.h
+++ QtDisplay/DashboardWidgets.h
@@ -1,82 +1,13 @@
-#pragma once
+#ifndef DASHBOARDWIDGETS_H
+#define DASHBOARDWIDGETS_H
+
 #include <QWidget>
-#include <QPropertyAnimation>
-#include <QTimer>
 
-class PulsingHeartWidget : public QWidget {
+class DashboardWidgets : public QWidget {
     Q_OBJECT
-    Q_PROPERTY(qreal scaleFactor READ scaleFactor WRITE setScaleFactor)
-    Q_PROPERTY(qreal glowOpacity READ glowOpacity WRITE setGlowOpacity)
+
 public:
-    explicit PulsingHeartWidget(QWidget *parent = nullptr);
-    qreal scaleFactor() const { return m_scaleFactor; }
-    void setScaleFactor(qreal scale);
-    qreal glowOpacity() const { return m_glowOpacity; }
-    void setGlowOpacity(qreal opacity);
-    void setDarkMode(bool dark);
-public slots:
-    void updateBpm(int bpm);
-protected:
-    void paintEvent(QPaintEvent *event) override;
-private:
-    qreal m_scaleFactor;
-    qreal m_glowOpacity;
-    bool m_isDarkMode;
-    QPropertyAnimation *singleBeatAnimation;
-    QPropertyAnimation *glowFadeAnimation;
-    QTimer *beatTimer;
+    explicit DashboardWidgets(QWidget *parent = nullptr);
 };
 
-class OxygenCircleWidget : public QWidget {
-    Q_OBJECT
-    Q_PROPERTY(qreal animatedValue READ animatedValue WRITE setAnimatedValue)
-public:
-    explicit OxygenCircleWidget(QWidget *parent = nullptr);
-    qreal animatedValue() const { return m_animatedValue; }
-    void setAnimatedValue(qreal val);
-    void setDarkMode(bool dark);
-public slots:
-    void updateSpo2(int spo2);
-protected:
-    void paintEvent(QPaintEvent *event) override;
-private:
-    qreal m_animatedValue;
-    bool m_isDarkMode;
-    QPropertyAnimation *valueAnimation;
-};
-
-class ActivityRingWidget : public QWidget {
-    Q_OBJECT
-    Q_PROPERTY(qreal animatedValue READ animatedValue WRITE setAnimatedValue)
-public:
-    explicit ActivityRingWidget(QWidget *parent = nullptr);
-    qreal animatedValue() const { return m_animatedValue; }
-    void setAnimatedValue(qreal val);
-    void setDarkMode(bool dark);
-public slots:
-    void updateValue(int val);
-protected:
-    void paintEvent(QPaintEvent *event) override;
-private:
-    qreal m_animatedValue;
-    bool m_isDarkMode;
-    QPropertyAnimation *valueAnimation;
-};
-
-class CalorieRingWidget : public QWidget {
-    Q_OBJECT
-    Q_PROPERTY(qreal animatedValue READ animatedValue WRITE setAnimatedValue)
-public:
-    explicit CalorieRingWidget(QWidget *parent = nullptr);
-    qreal animatedValue() const { return m_animatedValue; }
-    void setAnimatedValue(qreal val);
-    void setDarkMode(bool dark);
-public slots:
-    void updateValue(int val);
-protected:
-    void paintEvent(QPaintEvent *event) override;
-private:
-    qreal m_animatedValue;
-    bool m_isDarkMode;
-    QPropertyAnimation *valueAnimation;
-};
+#endif
--- QtDisplay/FitnessTrackerWindow.h
+++ QtDisplay/FitnessTrackerWindow.h
@@ -1,61 +1,14 @@
+#ifndef FITNESSTRACKERWINDOW_H
+#define FITNESSTRACKERWINDOW_H
+
 #pragma once
 #include <QWidget>
-#include <QLabel>
-#include <QStackedWidget>
-#include <QPushButton>
-#include <QComboBox>
-#include <QGraphicsOpacityEffect>
-#include "DashboardWidgets.h"
-#include "MockSensor.h"
 
 class FitnessTrackerWindow : public QWidget {
     Q_OBJECT
+
 public:
     explicit FitnessTrackerWindow(QWidget *parent = nullptr);
-
-private slots:
-    void onDailyUpdated(int bpm, int steps);
-    void onWorkoutUpdated(int seconds, int bpm, int spo2, double distance, int calories);
-    void toggleTheme();
-    void changeLanguage(int index);
-    void updateStaticTexts();
-    void fadeToPage(int pageIndex);
-    void formatTimeLabel(int seconds);
-
-private:
-    enum Language { EN, PL, DE };
-    Language m_currentLang;
-
-    QStackedWidget *stackedWidget;
-    QGraphicsOpacityEffect *opacityEffect;
-
-    QPushButton *themeButton;
-    QComboBox *langComboBox;
-    bool m_isDarkMode;
-    bool m_isWorkoutPaused;
-
-    // --- UI Elements ---
-    PulsingHeartWidget *homeHeartWidget;
-    ActivityRingWidget *homeStepsWidget;
-    QLabel *homeBpmLabel;
-    QLabel *homeStepsLabel;
-    QPushButton *startTrainingBtn;
-
-    QPushButton *runBtn, *cycleBtn, *swimBtn, *cancelSelectionBtn;
-
-    QLabel *workoutTimeLabel;
-    PulsingHeartWidget *workoutHeartWidget;
-    OxygenCircleWidget *workoutSpo2Widget;
-    QLabel *workoutBpmLabel;
-    QLabel *workoutSpo2Label;
-    QLabel *workoutDistanceLabel;
-    QLabel *workoutCalorieLabel;
-    QPushButton *pauseBtn, *endBtn, *homeBtn;
-
-    // Language references
-    QLabel *homeTitle, *homeHeartSub, *homeStepsSub;
-    QLabel *selectTitle;
-    QLabel *workTimeSub, *workHeartSub, *workSpo2Sub, *workDistSub, *workCalSub;
-
-    MockSensor *sensor;
 };
+
+#endif
